48ec129c854f40b0f460b406812e405dbbc61c95
Pasxal_Mubot <pascal_schmalen@hotmail.com>
2025-02-10 13:54:03 +0100

    service added, no crontab needed

---
+++ DataReview/review.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+a = pd.read_json('/Users/pascal/Documents/AccessBuildingClimate/DataReview/example/rasp4log.txt', lines=True)
+a['date'] = pd.to_datetime(a.time)
+a = a.set_index(a["date"]).sort_index()
+
+a.plot('date','temperature')
+plt.show()
