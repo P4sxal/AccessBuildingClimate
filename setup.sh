262a064a7bfc5ed90667191e3e4675e86b204610
Pasxal_Mubot <pascal_schmalen@hotmail.com>
2025-02-26 10:01:04 +0100

    systemd service and timer added and tested successfully

--- setup.sh
+++ setup.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
+#do not forget to make this script executable after coping
+#e.g. chmod +x setup.sh
 
 #add new user
-adduser --system mydongle
+/usr/sbin/adduser --system mydongle
 
 #copy SHT30_Software and change userrights
-chown -R mydongle /home/pi/SHT30_software
-mv /home/pi/SHT30_software/ /home/mydongle/
+/bin/mv /home/pi/SHT30_software/* /home/mydongle/
+/bin/chown -R mydongle /home/pi/SHT30_software
 
-echo "ok, lets continue"
+/bin/echo "user created and files copied"
 
 #copy service and timer file
-cp /home/mydongle/mydongle.service /etc/systemd/system
-cp /home/mydongle/mydongle.timer /etc/systemd/system
+/bin/cp /home/mydongle/mydongle.service /etc/systemd/system
+/bin/cp /home/mydongle/mydongle.timer /etc/systemd/system
 
-usermod -a -G gpio,i2c mydongle
-systemctl daemon-reload
-systemctl enable mydongle.timer
-systemctl start mydongle.timer
+/usr/sbin/usermod -a -G gpio,i2c mydongle
+/bin/systemctl daemon-reload
+/bin/systemctl enable mydongle.timer
+/bin/systemctl start mydongle.timer
+
+/bin/echo "AccessBuildingClimate Dongle activated"
